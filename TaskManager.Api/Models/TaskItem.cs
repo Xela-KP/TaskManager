62df29e288c5f7d5f6a2f0d5165e5494ad03c2f0
Alexander King Perocho <perocho.xela@gmail.com>
2025-05-23 23:15:10 -0400

    wired up database

---
+++ TaskManager.Api/Models/TaskItem.cs
@@ -0,0 +1,10 @@
+namespace TaskManager.Api.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TaskDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+app.MapGet("/tasks", async (TaskDbContext db) =>
+    await db.Tasks.ToListAsync());
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
